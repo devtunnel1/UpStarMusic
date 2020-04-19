6d172dccea05d5c6de1655706f77bb204ed81743
Akio Satoh <devtunnel@gmail.com>
2020-04-19 01:04:51 -0700

    Finished 74. The Artist Model

--- database/models/album.js
+++ database/models/album.js
@@ -1,1 +1,13 @@
-// Todo: create Album Schema
+const mongoose = require('mongoose')
+const Schema = mongoose.schema
+
+const AlbumSchema = new Schema({
+  title: String,
+  date: String,
+  copiesSold: Number,
+  numberTracks: Number,
+  image: String,
+  revenue: Number
+})
+
+module.exports = AlbumSchema
