f31fd5fd49d01500b564c3e6316f518d2253788f
Akio Satoh <devtunnel@gmail.com>
2020-04-19 01:44:42 -0700

    Finished 75. Finding Particular Records

--- database/models/album.js
+++ database/models/album.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
-const Schema = mongoose.schema
+const Schema = mongoose.Schema
 
 const AlbumSchema = new Schema({
   title: String,
-  date: String,
+  date: Date,
   copiesSold: Number,
   numberTracks: Number,
   image: String,
